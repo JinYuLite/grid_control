69057ea7a83dc66089a6538a6c707f4329b8398c
Yuan Zhang <zhangyuan2@laiye.com>
2021-08-26 01:04:11 +0800

    [add] env wrapper

---
+++ src/env_wrapper.py
@@ -0,0 +1,112 @@
+
+import gym 
+import numpy as np
+import copy
+
+class GridEnv(gym.Env):  
+    """Custom Environment that follows gym interface"""  
+    metadata = {'render.modes': ['human']}  
+
+    def __init__(self, env_inst):
+
+        super(GridEnv, self).__init__()    
+
+        # this is an instance of grid environment
+        self.env = env_inst
+
+        # Define action and observation space    
+        ACTION_DIM = 54 * 2
+        self.action_space = gym.spaces.Box(low=np.finfo(np.float32).min, high=np.finfo(np.float32).max, shape=(ACTION_DIM,), dtype=np.float32) 
+
+        tmp_obs = self.env.reset()
+        tmp_observation = vectorize(tmp_obs)
+        OBSERVATION_DIM = tmp_observation.shape[0]
+        self.action_space = gym.spaces.Box(low=np.finfo(np.float32).min, high=np.finfo(np.float32).max, shape=(OBSERVATION_DIM,), dtype=np.float32) 
+
+        print("Action Dim: {}\nObservation Dim :{}".format(ACTION_DIM, OBSERVATION_DIM))
+
+            
+    def step(self, action):    
+        act = unvectorize(action)
+        act = self._mask_act(act)
+
+        obs, reward, done, info = self.env.step(act)
+        observation = vectorize(obs)
+
+        # save legal act space for this step
+        self.legal_act_space = obs.action_space
+        return observation, reward, done, info  
+
+    def reset(self):
+        obs = self.env.reset()
+        observation = vectorize(obs)
+
+        # save legal act space for this step
+        self.legal_act_space = obs.action_space
+        return observation
+
+    def render(self, mode="human"):
+        return 
+
+    def close(self):
+        return
+
+    def _mask_act(self, act):
+
+        masked_act = {}
+        for k, v in self.legal_act_space.items():
+            v_low, v_high = v.low, v.high
+            real_v = copy.deepcopy(act[k])
+            real_v = np.where(real_v>v_low, real_v, v_low)
+            real_v = np.where(real_v<v_high, real_v, v_high)
+            masked_act[k] = real_v
+
+        adjust_gen_p_low = self.legal_act_space["adjust_gen_p"].low
+        adjust_gen_p_high = self.legal_act_space["adjust_gen_p"].low
+        adjust_gen_v_low = self.legal_act_space["adjust_gen_v"].low
+        adjust_gen_p_high = self.legal_act_space["adjust_gen_p"].low
+        return masked_act
+
+
+def vectorize(obs):
+    """
+        Convert real obs to gym-like obs
+    """
+    new_obs = []
+    # gen_p, gen_q, gen_v: 机组发电
+    new_obs.extend(obs.gen_p)
+    new_obs.extend(obs.gen_q)
+    new_obs.extend(obs.gen_v)
+
+    # gen status, steps_to_recover_gen, steps_to_close_gen: 机组状态, int类型
+    new_obs.extend(list(obs.gen_status))
+    new_obs.extend(list(obs.steps_to_recover_gen))
+    new_obs.extend(list(obs.steps_to_close_gen))
+
+    # curstep_renewable_gen_p_max, nextstep_newable_gen_p_max: 新能源发电
+    new_obs.extend(obs.curstep_renewable_gen_p_max)
+    new_obs.extend(obs.nextstep_renewable_gen_p_max)
+
+    # load_p, load_q, load_v, nextstep_load_p: 负荷耗电
+    new_obs.extend(obs.load_p)
+    new_obs.extend(obs.load_q)
+    new_obs.extend(obs.load_v)
+    new_obs.extend(obs.nextstep_load_p)
+
+    # 线路信息, 节点信息
+
+    # M: 各个特征是否需要归一化
+    observation = np.array(new_obs)
+    return observation
+
+
+def unvectorize(action):
+    """
+        Convert gym-like act to real act
+    """
+    action = np.array(action).flatten().astype(np.float32)
+    act_dim = action.shape[0] // 2
+    act = {"adjust_gen_p": action[:act_dim],
+           "adjust_gen_v": action[act_dim:]}
+    return act
+
--- src/main.py
+++ src/main.py
@@ -6,11 +6,15 @@ from Agent.RandomAgent import RandomAgent
 from Environment.base_env import Environment
 from utilize.settings import settings
 
+from env_wrapper import GridEnv
+
 def run_task(my_agent):
+
+    env_inst = Environment(settings, "EPRIReward")
+    env = GridEnv(env_inst) 
 
     for episode in range(max_episode):
         print('\n------ episode ', episode)
-        env = Environment(settings, "EPRIReward")
         print('------ reset ')
         obs = env.reset()
 
@@ -21,12 +25,18 @@ def run_task(my_agent):
         for timestep in range(max_timestep):
             print('------ step ', timestep)
 
-            ids = [i for i,x in enumerate(obs.rho) if x > 1.0]
-            print("gen p: ", obs.gen_p[:10])
+            # for raw environment
+            # ids = [i for i,x in enumerate(obs.rho) if x > 1.0]
+            # print("gen p: ", obs.gen_p[:10])
             # print("overflow rho: ", [obs.rho[i] for i in ids])    
 
-            action = my_agent.act(obs, reward, done)
-            print("adjust_gen_p: ", action['adjust_gen_p'][:10])
+            # action = my_agent.act(obs, reward, done)
+            # print("adjust_gen_p: ", action['adjust_gen_p'][:10])
+
+            # for wrapped environment
+            print(obs.shape)
+            action = np.random.random(108).astype(np.float32)
+            print(action.shape)
 
             obs, reward, done, info = env.step(action)
             print('info:', info)
@@ -34,7 +44,7 @@ def run_task(my_agent):
                 break
 
 if __name__ == "__main__":
-    max_timestep = 3  # 最大时间步数
+    max_timestep = 10  # 最大时间步数
     max_episode = 2  # 回合数
 
     my_agent = RandomAgent(settings.num_gen)
