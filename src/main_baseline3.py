e0248245e325de567851123afca9183475e15f3f
hsvgbkhgbv <wangjianhong1993@gmail.com>
2021-09-01 01:12:52 +0100

    a script for baseline3

--- src/main_baseline3.py
+++ src/main_baseline3.py
@@ -5,7 +5,7 @@ from Agent.DoNothingAgent import DoNothingAgent
 from Agent.RandomAgent import RandomAgent
 from Environment.base_env import Environment
 from utilize.settings import settings
-from stable_baselines3 import PPO, DDPG
+from stable_baselines3 import PPO, DDPG, TD3, SAC
 from env_wrapper import GridEnv
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
@@ -47,11 +47,38 @@ if __name__ == "__main__":
     env = Environment(settings, "EPRIReward")
     env = GridEnv(env)
 
-    n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+    # n_actions = env.action_space.shape[-1]
+    # action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.01 * np.ones(n_actions))
 
-    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+    # model = TD3("MlpPolicy", 
+    #             env, 
+    #             action_noise=action_noise, 
+    #             verbose=1, 
+    #             learning_rate=0.001,
+    #             buffer_size=5000,
+    #             learning_starts=100,
+    #             batch_size=128,
+    #             policy_delay=5
+    #         )
+    model = SAC(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        buffer_size=100000,
+        train_freq=50,
+        gamma=0.95,
+        batch_size=128,
+        learning_rate=0.0005,
+        target_update_interval=100,
+        gradient_steps=10
+    )
 
-    model.learn(total_timesteps=1000, log_interval=10)
+    model.learn(total_timesteps=2000000, log_interval=10)
+
+    model.save("sac_trial")
+
+    del model
+
+    model = SAC.load("sac_trial")
 
     run_test(model)
